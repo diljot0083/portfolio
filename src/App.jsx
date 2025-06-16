d99851c56f5942fb508a03f04c5e8b5c55ee2f4c
Diljot Singh <diljot00083@gmail.com>
2025-06-16 15:57:18 +0530

    Add Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <Router>
       <AppRoutes />
+      <Analytics />
     </Router>
   );
 }
